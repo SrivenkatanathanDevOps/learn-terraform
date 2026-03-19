e53a94e7c4b71e5413d6e2d5717e89ec8df98668
SrivenkatanathanDevOps <srivenkat2206@gmail.com>
2026-03-19 13:24:25 +0530

    Adding ec2 resource files

---
+++ ec2 resource/main.tf
@@ -0,0 +1,14 @@
+resource "ec2_instance" "frontend" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "Frontend"
+  }
+
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
